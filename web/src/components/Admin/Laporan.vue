eab0fe2988cc19140277ea3df2ca399651704452
HansGio <hansgio007@gmail.com>
2020-12-10 23:21:30 +0700

    push admin

--- web/src/components/Admin/Laporan.vue
+++ web/src/components/Admin/Laporan.vue
@@ -1,110 +1,135 @@
 <template>
-    <v-main class="list">
-        <v-data-table :headers="headers" :items="orders" :search="search">
-            <!-- Kurang tampilan for didalam item -->
-            <!-- <template v-slot:[`item.name`]>
-                <div
-                    v-for="order in order_detail"
-                    :key="order"
-                    cols="12"
-                    xs="1"
-                    sm="6"
-                    md="2"
-                >
-                    {{ this.order.item_name }}
-                    <br />
-                </div>
-            </template> -->
-        </v-data-table>
-    </v-main>
+	<v-main class="list">
+		<v-data-table :headers="headers" :items="orders" :search="search">
+			<template v-slot:[`item.statusPesanan`]="{ item }">
+				<div class="pa-4">
+					<v-chip :color="getColor(item.delivery_status)" dark>
+						{{ item.delivery_status }}
+					</v-chip>
+				</div>
+			</template>
+
+			<template v-slot:[`item.harga`]="{ item }">
+				<div>
+					<h5 class="font-weight-bold">Rp. {{ getTotal(item) }}</h5>
+				</div>
+			</template>
+
+			<template v-slot:[`item.productInfo`]="{ item }">
+				<div class="p-2">
+					<div v-for="(detail, index) in item.order_details" :key="index">
+						<p>
+							{{
+								index +
+									1 +
+									". Item Name: " +
+									item.order_details[index].item_name +
+									", Qty: " +
+									item.order_details[index].item_quantity +
+									", Size: " +
+									item.order_details[index].item_size +
+									", Price: Rp. " +
+									item.order_details[index].buy_price
+							}}
+						</p>
+					</div>
+				</div>
+			</template>
+			<template v-slot:[`item.userInfo`]="{ item }">
+				<div class="p-2 d-flex align-center">
+					<div class="ml-3">
+						<span class="h6">{{ item.recipient_name }}</span>
+						<v-spacer></v-spacer>
+						{{ item.recipient_phone }}
+						<v-spacer></v-spacer>
+						{{ item.recipient_address }}
+					</div>
+				</div>
+			</template>
+		</v-data-table>
+	</v-main>
 </template>
 <script>
 export default {
-    name: "List",
-    data() {
-        return {
-            snackbar: false,
-            error_message: "",
-            color: "",
-            search: null,
-            dialog: false,
-            dialogConfirm: false,
-            products: [],
-            form: {
-                nama_produk: null,
-                satuan: null,
-                harga_jual: null,
-                stok: null,
-            },
-            deleteId: "",
-            editId: "",
-            orders: [],
-            order_detail: [],
-            headers: [
-                {
-                    text: "No Pesanan",
-                    align: "start",
-                    sortable: false,
-                    value: "id",
-                },
-                { text: "Nama Pembeli", value: "recipient_name" },
-                { text: "Nama Barang", value: "name" },
-                { text: "Alamat", value: "recipient_address" },
-                { text: "Status", value: "delivery_status" },
-            ],
-            desserts: [],
-        };
-    },
-    methods: {
-        close() {
-            this.dialog = false;
-        },
-        cancel() {
-            this.dialog = false;
-        },
-        getColor(status) {
-            if (status == "aktif") return "green";
-            else return "red";
-        },
-        readDataOrderDetail() {
-            var url = this.$api + "/order-detail";
-            this.$http
-                .get(url, {
-                    headers: {
-                        Authorization:
-                            "Bearer " + localStorage.getItem("token"),
-                    },
-                })
-                .then((response) => {
-                    this.order_detail = response.data.items;
-                });
-        },
-        readDataOrder() {
-            var url = this.$api + "/order";
-            this.$http
-                .get(url, {
-                    headers: {
-                        Authorization:
-                            "Bearer " + localStorage.getItem("token"),
-                    },
-                })
-                .then((response) => {
-                    this.orders = response.data.items;
-                });
-        },
-    },
-    mounted() {
-        this.readDataOrder();
-        this.readDataUser();
-        for (this.product in this.products) {
-            console.log(this.product);
-        }
-    },
+	name: "List",
+	data() {
+		return {
+			snackbar: false,
+			error_message: "",
+			color: "",
+			search: null,
+			dialog: false,
+			dialogConfirm: false,
+			order: new FormData(),
+			orders: [],
+			form: {
+				nama_produk: null,
+				satuan: null,
+				harga_jual: null,
+				stok: null,
+			},
+			deleteId: "",
+			editId: "",
+			headers: [
+				{
+					text: "No Pesanan",
+					align: "start",
+					sortable: false,
+					value: "id",
+				},
+				{ text: "Informasi Produk", value: "productInfo" },
+				{ text: "Informasi Penerima", value: "userInfo" },
+				{ text: "Total Harga (Rp)", value: "harga" },
+				{ text: "Status", value: "statusPesanan" },
+			],
+			desserts: [],
+		};
+	},
+	methods: {
+		getTotal(item) {
+			let total = 0;
+			item.order_details.forEach((element) => {
+				total += parseFloat(element.buy_price) * parseInt(element.item_quantity);
+			});
+
+			return total;
+		},
+		getColor(status) {
+			if (status == "Terkirim") return "green";
+			else if (status == "Dibatalkan") return "red";
+		},
+		readData() {
+			var url = this.$api + "/order";
+			this.$http
+				.get(url, {
+					headers: {
+						Authorization: "Bearer " + localStorage.getItem("token"),
+					},
+				})
+				.then((response) => {
+					console.log(response);
+					this.orders = response.data.items;
+					this.orders = this.orders.filter(
+						(e) =>
+							e.delivery_status === "Terkirim" || e.delivery_status === "Dibatalkan"
+					);
+				});
+		},
+		close() {
+			this.dialog = false;
+		},
+		cancel() {
+			this.dialog = false;
+		},
+	},
+	mounted() {
+		this.readData();
+	},
 };
 </script>
 
 <style>
 transparent {
-    background-color: transparent !important;
+	background-color: transparent !important;
 }
 </style>
--- web/src/components/Admin/Voucher.vue
+++ web/src/components/Admin/Voucher.vue
@@ -1,129 +1,68 @@
 <template>
-    <v-main class="list">
-        <v-data-table :headers="headers" :items="desserts" :search="search">
-            <template v-slot:item.pengaturan>
-                <v-icon large color="darken-2" @click="dialog = true">
-                    mdi-trash-can-outline
-                </v-icon>
+	<v-main class="list">
+		<v-data-table :headers="headers" :items="desserts" :search="search">
+			<template v-slot:[`item.pengaturan`]>
+				<v-icon large color="darken-2" @click="dialog = true">
+					mdi-trash-can-outline
+				</v-icon>
 
-                <v-icon large color="darken-2" @click="checkDialog = true">
-                    mdi-eyedropper-variant
-                </v-icon>
-            </template>
-            <template
-                v-slot:[`item.voucherPrice`]="{
-                    item,
-                }"
-            >
-                <div>
-                    <p class="font-weight-bold">Rp. {{ item.voucherPrice }}</p>
-                </div>
-            </template>
-        </v-data-table>
-    </v-main>
+				<v-icon large color="darken-2" @click="checkDialog = true">
+					mdi-eyedropper-variant
+				</v-icon>
+			</template>
+			<template
+				v-slot:[`item.voucherPrice`]="{
+					item,
+				}"
+			>
+				<div>
+					<p class="font-weight-bold">Rp. {{ item.voucherPrice }}</p>
+				</div>
+			</template>
+		</v-data-table>
+	</v-main>
 </template>
 <script>
 export default {
-    name: "List",
-    data() {
-        return {
-            snackbar: false,
-            error_message: "",
-            color: "",
-            search: null,
-            dialog: false,
-            dialogConfirm: false,
+	name: "List",
+	data() {
+		return {
+			snackbar: false,
+			error_message: "",
+			color: "",
+			search: null,
+			dialog: false,
+			dialogConfirm: false,
 
-            product: new FormData(),
-            products: [],
-            form: {
-                nama_produk: null,
-                satuan: null,
-                harga_jual: null,
-                stok: null,
-            },
-            deleteId: "",
-            editId: "",
-            headers: [
-                {
-                    text: "No Kode Voucher",
-                    align: "start",
-                    sortable: false,
-                    value: "no",
-                },
-                { text: "Nama Voucher", value: "namaVoucher" },
-                { text: "Kode Voucher", value: "kodeVoucher" },
-                { text: "Voucher", value: "voucherPrice" },
-                { text: "action", value: "pengaturan" },
-            ],
-            desserts: [
-                {
-                    no: "1",
-                    name: "ayam",
-                    image: "aa",
-                    size: "s,m,l",
-                    harga: 200000,
-                    stock: 20,
-                    namaVoucher:
-                        "cukup follow ig tolanda dapatkan potongan 100%",
-                    noHp: "08XXXXNERAKAXXX",
-                    alamat: "Jln. Neraka seriusan, gg Gamping tukngku api",
-                    resi: "tol 13 0 D 0 H",
-                    kodeVoucher: "TOL1818",
-                    voucherPrice: 100000,
-                },
-                {
-                    no: "2",
-                    name: "babi",
-                    image: "aa",
-                    size: "s,m,l",
-                    harga: 100000,
-                    stock: 10,
-                    status: "non aktif",
-                    namaVoucher:
-                        "cukup follow ig tolanda dapatkan potongan 100%",
-                    noHp: "08XXXXNERAKAXXX",
-                    alamat: "Jln. Neraka seriusan, gg Gamping tukngku api",
-                    resi: "tol 13 0 D 0 H",
-                    kodeVoucher: "TOL1818",
-                    voucherPrice: 100000,
-                },
-                {
-                    no: "3",
-                    name: "kaki babi",
-                    image: "aa",
-                    size: "s,m,l",
-                    harga: 100000,
-                    stock: 10,
-                    status: "non aktif",
-                    namaVoucher:
-                        "cukup follow ig tolanda dapatkan potongan 100%",
-                    noHp: "08XXXXNERAKAXXX",
-                    alamat: "Jln. Neraka seriusan, gg Gamping tukngku api",
-                    resi: "tol 13 0 D 0 H",
-                    kodeVoucher: "TOL1818",
-                    voucherPrice: 100000,
-                },
-            ],
-        };
-    },
-    methods: {
-        close() {
-            this.dialog = false;
-        },
-        cancel() {
-            this.dialog = false;
-        },
-        getColor(status) {
-            if (status == "aktif") return "green";
-            else return "red";
-        },
-    },
+			voucher: new FormData(),
+			vouchers: [],
+			form: {
+				nama_produk: null,
+				satuan: null,
+				harga_jual: null,
+				stok: null,
+			},
+			deleteId: "",
+			editId: "",
+		};
+	},
+	methods: {
+		close() {
+			this.dialog = false;
+		},
+		cancel() {
+			this.dialog = false;
+		},
+		getColor(status) {
+			if (status == "aktif") return "green";
+			else return "red";
+		},
+	},
 };
 </script>
 
 <style>
 transparent {
-    background-color: transparent !important;
+	background-color: transparent !important;
 }
 </style>
