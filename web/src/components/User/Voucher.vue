a2ec01a1d22d8c09cd9c2687103e760a4ac77cac
KevinGhebre <kevinghebre20@gmail.com>
2020-12-10 17:27:33 +0700

    fix voucher errorr

--- web/src/components/User/Voucher.vue
+++ web/src/components/User/Voucher.vue
@@ -1,30 +1,35 @@
 <template>
-  <v-row class="grey lighten-5 ml-4">
-    <v-col cols="12" md="3" sm="6" v-for="">
-        <v-card class="mx-auto" max-width="344" outlined  style="background-color:#FFBE76">
-            <v-card-actions>
-                <v-icon large  @click=""> mdi-plus-circle-outline</v-icon>
-            </v-card-actions>
-            
-            <v-list-item-content>
-                <v-list-item-title class="headline text-center"> Headline 5 {{}} </v-list-item-title>
-                <hr>
-                <v-list-item-subtitle class="text-center">Kode Voucher</v-list-item-subtitle>
-                <v-list-item-title class="headline text-center">adas{{}} </v-list-item-title>
-            </v-list-item-content>
+    <v-row class="grey lighten-5 ml-4">
+        <v-col cols="12" md="3" sm="6">
+            <v-card
+                class="mx-auto"
+                max-width="344"
+                outlined
+                style="background-color:#FFBE76"
+            >
+                <v-card-actions>
+                    <v-icon large> mdi-plus-circle-outline</v-icon>
+                </v-card-actions>
 
-            </v-list-item>
-
-        </v-card>
-    </v-col>
- </v-row> 
-
+                <v-list-item-content>
+                    <v-list-item-title class="headline text-center">
+                        Headline 5 {{}}
+                    </v-list-item-title>
+                    <hr />
+                    <v-list-item-subtitle class="text-center"
+                        >Kode Voucher</v-list-item-subtitle
+                    >
+                    <v-list-item-title class="headline text-center"
+                        >adas{{}}
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
-    export default{
-        data : () => ({
-
-        })
-    }
+export default {
+    data: () => ({}),
+};
 </script>
